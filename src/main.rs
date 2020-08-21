5135564aaab89f6525dc223a7f242c8d4c0313dd
IceSentry <c.giguere42@gmail.com>
2020-08-21 16:13:53 -0400

    add demo ui

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,28 @@ mod egui_plugin;
 mod painter;
 
 use bevy::prelude::*;
-use egui_plugin::EguiPlugin;
+use egui::label;
+use egui_plugin::{EguiContext, EguiPlugin};
+
+fn egui_demo_system(mut ctx: ResMut<EguiContext>) {
+    if let Some(ui) = ctx.ui.as_mut() {
+        ui.collapsing("About Egui", |ui| {
+            ui.add(label!(
+                "Egui is an experimental immediate mode GUI written in Rust."
+            ));
+
+            ui.horizontal(|ui| {
+                ui.label("Project home page:");
+                ui.hyperlink("https://github.com/emilk/egui");
+            });
+        });
+    }
+}
 
 fn main() {
     App::build()
         .add_default_plugins()
         .add_plugin(EguiPlugin)
+        .add_system(egui_demo_system.system())
         .run();
 }
